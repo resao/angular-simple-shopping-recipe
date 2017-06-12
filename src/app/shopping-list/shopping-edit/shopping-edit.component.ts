f59e28b43ed631a7c1c64536534676237a8a70b3
Resao <mail@christophermiles.co.uk>
2017-06-12 19:26:39 +0100

    Added ability to add new items to shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../../shared/ingredient.model';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  ingredientName: string;
+  ingredientAmount: number;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const newIngredient = new Ingredient(this.ingredientName, this.ingredientAmount);
+    console.log(newIngredient);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
