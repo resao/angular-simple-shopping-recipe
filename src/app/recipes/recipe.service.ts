537388514867bab342d7af0a4386f376eb65b0e5
Resao <mail@christophermiles.co.uk>
2017-06-13 19:49:11 +0100

    Add ability to add ingredients to the shopping list from our recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+
+@Injectable()
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a test description',
-    'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('Another test Recipe', 'This is another test description',
-    'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe(
+      'A test Recipe',
+      'This is a test description',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('grass', 3)
+      ]),
+    new Recipe(
+      'Another test Recipe',
+      'This is another test description',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Bread', 1),
+        new Ingredient('Butter', 3)
+      ]),
   ];
+
+  constructor(private slService: ShoppingListService){
+
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
