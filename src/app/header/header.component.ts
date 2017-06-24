570bd6427ceec5dd5d22bbcdacea41ab6e39b24e
Resao <mail@christophermiles.co.uk>
2017-06-24 18:31:14 +0100

    Enable recipes to be saved from the header save button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {DataStorageService} from './../shared/data-storage.service';
+import {Component} from '@angular/core';
+import {Response} from '@angular/http';
 
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
-})
+@Component({selector: 'app-header', templateUrl: './header.component.html', styleUrls: ['./header.component.css']})
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+  onSaveClick() {
+    this.onSaveRecipes();
+  }
 
+  onSaveRecipes() {
+    this
+      .dataStorageService
+      .storeRecipes()
+      .subscribe((response: Response) => {
+        console.log(response);
+      });
+  }
 }
