d157cdc3ca7a3207576cf1c6426b0617dba1e0fc
Resao <mail@christophermiles.co.uk>
2017-06-24 18:52:51 +0100

    Add ability to get recipes from API and set these in the recipe service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
@@ -10,5 +11,14 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put(this.API_URL, this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get(this.API_URL).subscribe(
+      (response: Response) => {
+        const recipes: Recipe[] = response.json();
+        this.recipeService.setRecipes(recipes);
+      }
+    );
   }
 }
