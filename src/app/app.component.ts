f8961c8098200bd18fb0d3d59a6ce8e9019e17a1
Christopher Miles <mail@christophermiles.co.uk>
2018-05-20 12:15:28 +0100

    Add and configure firebase SDK for auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB7PTdvK6rRopjc0UiM7b03_6z33vquPEU',
+      authDomain: 'ng-recipe-book-c67db.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,3 @@
-import { RecipeService } from './recipe.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
