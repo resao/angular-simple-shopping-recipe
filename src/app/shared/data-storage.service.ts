8eff2799076457822551e77667f3ae846bc437e2
Christopher Miles <mail@christophermiles.co.uk>
2018-05-27 19:01:09 +0100

    Change data storage to use auth store instead of service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,47 +1,50 @@
-import { Recipe } from './../recipes/recipe.model';
-import { RecipeService } from './../recipes/recipe.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { switchMap } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import { Recipe } from './../recipes/recipe.model';
+import { RecipeService } from './../recipes/recipe.service';
 import { environment as env } from '../../environments/environment';
-import { AuthService } from '../auth/auth.service';
+import * as fromAuth from '../auth/store/auth.reducers';
+import * as fromApp from '../store/app.reducers';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(
-    private authService: AuthService,
+    private store: Store<fromApp.AppState>,
     private http: HttpClient,
     private recipeService: RecipeService) {}
 
   storeRecipes() {
-    const token = this.authService.getToken();
-
-    return this.http.put(env.api_url, this.recipeService.getRecipes(), {
-      params: new HttpParams().set('auth', token)
-    });
+    return this.store.select('auth').pipe(switchMap((authState: fromAuth.State) => {
+      return this.http.put(env.api_url, this.recipeService.getRecipes(), {
+        params: new HttpParams().set('auth', authState.token)
+      });
+    }))
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
-    this.http.get(env.api_url, {
-      params: new HttpParams().set('auth', token)
-    })
-    .pipe(map(
-      (recipes: Recipe[]) => {
-        for (const recipe of recipes){
-          if (!recipe['ingredients']) {
-            recipe['ingredients'] = [];
+    this.store.select('auth').pipe(map((authState: fromAuth.State) => {
+      return this.http.get(env.api_url, {
+        params: new HttpParams().set('auth', authState.token)
+      }).pipe(map(
+        (recipes: Recipe[]) => {
+          for (const recipe of recipes){
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
           }
+          return recipes;
         }
-        return recipes;
-      }
-    ))
-    .subscribe(
-      (recipes: Recipe[]) => {
-        this.recipeService.setRecipes(recipes);
-      }
-    );
+      )).subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      )
+    }))
   }
 }
