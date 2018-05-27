3c763488d6c2888af5d679a90c463e18dee77be3
Christopher Miles <mail@christophermiles.co.uk>
2018-05-27 19:07:05 +0100

    Updated recipe list and detail to use store

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { AppConfig as config } from '../../app.config';
-import { AuthService } from '../../auth/auth.service';
 import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
+
 import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -17,15 +20,16 @@ import * as fromApp from '../../store/app.reducers';
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
+  authState: Observable<fromAuth.State>;
 
   constructor(
-    public authService: AuthService,
     private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState = this.store.select('auth');
     const id = this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
-import { Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { AppConfig as config } from '../../app.config';
 import { RecipeService } from './../recipe.service';
 import { Recipe } from '../recipe.model';
-import { AppConfig as config } from '../../app.config';
-import { AuthService } from '../../auth/auth.service';
+
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,14 +19,16 @@ import { AuthService } from '../../auth/auth.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
   subscription: Subscription;
+  authState: Observable<fromAuth.State>;
 
   constructor(
-    public authService: AuthService,
     private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState = this.store.select('auth');
     this.recipes = this.recipeService.getRecipes();
     this.subscription = this.recipeService.recipesChanged.subscribe(
       (recipes: Recipe[]) => {
