9689d3cd119d35dab28fc26ee7a6e2a8d6a8a8ab
Christopher Miles <mail@christophermiles.co.uk>
2018-05-28 13:10:46 +0100

    Removing redundant sign in / up from authService and firebase init moved to core

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,54 +10,6 @@ import * as AuthActions from './store/auth.actions';
 export class AuthService {
 
   constructor(private router: Router, private store: Store<fromApp.AppState>) {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyB7PTdvK6rRopjc0UiM7b03_6z33vquPEU',
-      authDomain: 'ng-recipe-book-c67db.firebaseapp.com'
-    });
-
-    // If the user signs in, set the token
-    /*firebase.auth().onAuthStateChanged(() => {
-      if (firebase.auth().currentUser) {
-        this.getToken();
-      }
-    });*/
-  }
-
-  signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(
-        user => {
-          this.store.dispatch(new AuthActions.Signup());
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => {
-                this.store.dispatch(new AuthActions.SetToken(token));
-              }
-            )
-        }
-      )
-      .catch(
-        error => console.log(error)
-      )
-  }
-
-  signinUser(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-        response => {
-          this.store.dispatch(new AuthActions.Signin());
-          this.router.navigate(['/']);
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => {
-                this.store.dispatch(new AuthActions.SetToken(token));
-              }
-            )
-        }
-      )
-      .catch(
-        error => console.log(error)
-      )
   }
 
   logout() {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import * as firebase from 'firebase';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +9,18 @@ import { AppRoutingModule } from '../app-routing.module';
 import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { AuthService } from '../auth/auth.service';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB7PTdvK6rRopjc0UiM7b03_6z33vquPEU',
+  authDomain: 'ng-recipe-book-c67db.firebaseapp.com'
+});
+
+// If the user signs in, set the token
+/*firebase.auth().onAuthStateChanged(() => {
+  if (firebase.auth().currentUser) {
+    this.getToken();
+  }
+});*/
 
 @NgModule({
   declarations: [
