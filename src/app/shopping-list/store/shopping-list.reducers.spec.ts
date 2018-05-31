5aad5f2ede85fe09d40567591a9e3406239db057
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 19:47:00 +0100

    Add test for stop edit reducer

--- src/app/shopping-list/store/shopping-list.reducers.spec.ts
+++ src/app/shopping-list/store/shopping-list.reducers.spec.ts
@@ -1,14 +1,40 @@
 import * as fromShoppingList from './shopping-list.reducers';
 import * as fromActions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
 
 describe('ShoppingListReducer', () => {
+  const { initialState } = fromShoppingList;
+
+  const ingredient: Ingredient = {
+    name: 'Bananas',
+    amount: 20
+  };
+
   describe('undefined action', () => {
     it('should return the default state', () => {
-      const { initialState } = fromShoppingList;
       const action = {} as any;
       const state = fromShoppingList.shoppingListReducer(undefined, action);
 
       expect(state).toBe(initialState);
     });
-  })
+  });
+
+  describe('Ingredient Actions', () => {
+  });
+
+  describe('Edit Actions', () => {
+    describe('STOP_EDIT action', () => {
+      it('should reset edited ingredient and index', () => {
+        const previousState: fromShoppingList.State = {
+          ...initialState,
+          editedIngredient: {...ingredient},
+          editedIngredientIndex: 2
+        };
+        const action = new fromActions.StopEdit();
+        const state = fromShoppingList.shoppingListReducer(previousState, action);
+
+        expect(state).toEqual(initialState);
+      });
+    })
+  });
 });
