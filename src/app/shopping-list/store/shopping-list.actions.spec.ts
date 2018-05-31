42d0f4985ab2d1cb356461afccca74b5179d9c8f
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 19:15:02 +0100

    Added remaining tests for shopping list actions

--- src/app/shopping-list/store/shopping-list.actions.spec.ts
+++ src/app/shopping-list/store/shopping-list.actions.spec.ts
@@ -23,13 +23,61 @@ describe('Shopping List Actions', () => {
 
         expect({ ...action }).toEqual({
           type: fromShoppingList.ADD_INGREDIENT,
-          payload,
+          payload
+        });
+      });
+    });
+
+    describe('AddIngredients', () => {
+      it('should create an action', () => {
+        const payload = [ ...ingredients];
+        const action = new fromShoppingList.AddIngredients(payload);
+
+        expect({ ...action }).toEqual({
+          type: fromShoppingList.ADD_INGREDIENTS,
+          payload
+        });
+      });
+    });
+
+    describe('UpdateIngredient', () => {
+      it('should create an action', () => {
+        const payload = {ingredient: { ...ingredient }};
+        const action = new fromShoppingList.UpdateIngredient(payload);
+
+        expect({ ...action }).toEqual({
+          type: fromShoppingList.UPDATE_INGREDIENT,
+          payload
+        });
+      });
+    });
+
+    describe('DeleteIngredient', () => {
+      it('should create an action', () => {
+        const payload = 1;
+        const action = new fromShoppingList.DeleteIngredient(payload);
+
+        expect({ ...action }).toEqual({
+          type: fromShoppingList.DELETE_INGREDIENT,
+          payload
         });
       });
     });
   });
 
   describe('Edit Actions', () => {
+    describe('StartEdit', () => {
+      it('should create an action', () => {
+        const payload = 1;
+        const action = new fromShoppingList.StartEdit(payload);
+
+        expect({ ...action }).toEqual({
+          type: fromShoppingList.START_EDIT,
+          payload
+        });
+      });
+    });
+
     describe('StopEdit', () => {
       it('should create an action', () => {
         const action = new fromShoppingList.StopEdit();
