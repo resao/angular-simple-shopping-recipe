2efdf0c1936e9ebd05bb307e78853635422ad86d
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 19:05:53 +0100

    Add test for add ingredient action

--- src/app/shopping-list/store/shopping-list.actions.spec.ts
+++ src/app/shopping-list/store/shopping-list.actions.spec.ts
@@ -1,8 +1,34 @@
 import * as fromShoppingList from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+const ingredient = {
+  name: 'Tomatoes',
+  amount: 5
+};
+
+const ingredients: Ingredient[] = [{
+  name: 'Tomatoes',
+  amount: 5
+}, {
+  name: 'Apples',
+  amount: 10
+}];
 
 describe('Shopping List Actions', () => {
   describe('Ingredient Actions', () => {
+    describe('AddIngredient', () => {
+      it('should create an action', () => {
+        const payload = { ...ingredient };
+        const action = new fromShoppingList.AddIngredient(payload);
+
+        expect({ ...action }).toEqual({
+          type: fromShoppingList.ADD_INGREDIENT,
+          payload,
+        });
+      });
+    });
   });
+
   describe('Edit Actions', () => {
     describe('StopEdit', () => {
       it('should create an action', () => {
